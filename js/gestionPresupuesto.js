d55ac08c2b675be6287645cf36c0d0fa492b5200
wali123ker <walid.kerboub@colegiomiralmonte.es>
2025-11-26 15:43:50 +0100

    Add initial implementation of personal budget manager application
    - Created README.md to outline project structure and development steps.
    - Added CSS styles for the application interface in estilos.css.
    - Implemented JavaScript functions for budget management in gestionPresupuesto.js, including budget updating and expense creation.
    - Set up package.json with project metadata and testing scripts using Mocha and Chai.
    - Developed unit tests for budget management functions in 01_testIntroJSI.js, 02_testIntroJSII.js, and 03_testIntroJSIII.js, covering various functionalities such as expense creation, filtering, and grouping.

---
+++ js/gestionPresupuesto.js
@@ -0,0 +1,24 @@
+// TODO: Variables globales
+
+
+// TODO: Funciones adicionales
+
+
+function actualizarPresupuesto() {
+    // TODO
+}
+
+function mostrarPresupuesto() {
+    // TODO
+}
+
+function CrearGasto() {
+    // TODO
+}
+
+// Exportación de funciones
+export   {
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto
+}
